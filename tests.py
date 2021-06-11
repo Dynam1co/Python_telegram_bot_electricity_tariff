06a07da47f838ec64299a32ff4743fb9fb833261
Francisco Javier Asensi Benito <asensi.benito@gmail.com>
2021-06-11 17:57:16 +0200

    Check tariffs web online

--- tests.py
+++ tests.py
@@ -1,7 +1,14 @@
 import requests
+import conf_management as Conf
 
 
 def test_have_connection():
     resp = requests.get('https://google.es')
     
+    assert resp.status_code == 200
+
+
+def test_tariff_url_runs():
+    resp = requests.get(Conf.get_source())
+
     assert resp.status_code == 200
